b7f1d194febc4322da5cf5ea49bc488769e574fc
Minou <am.mihoubi@gmail.com>
2024-02-09 15:36:18 +0100

    03 Login users

--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages 
 
 # Create your views here.
 def home(request):
     context = {'title':'Home'}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        context['test'] = request.POST
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged In")
+        else:
+            messages.warning(request, "There was an error, please try again!")
+        return redirect('home')
     return render(request, 'crm/home.html', context)
