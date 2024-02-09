0a812f4285e6464835a966d3db26fe9d51e5f709
Minou <am.mihoubi@gmail.com>
2024-02-09 22:43:40 +0100

    04 Logout users

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 
 # Create your views here.
 def home(request):
     context = {'title':'Home'}
+    context['request.method'] = request.method
+    context['request'] = request
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -12,8 +14,17 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You have been logged In")
+            messages.success(request, "You have been logged In.")
         else:
             messages.warning(request, "There was an error, please try again!")
         return redirect('home')
+    
     return render(request, 'crm/home.html', context)
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged Out.")
+    return redirect('home')
+
+
+
