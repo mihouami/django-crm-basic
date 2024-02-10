22c7f44a533c457a872d397883bd5b9deb070157
Minou <am.mihoubi@gmail.com>
2024-02-10 20:08:10 +0100

    05 Register users

---
+++ crm/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import EmailField, CharField, TextInput
+
+class UserRegisterForm(UserCreationForm):
+    email = EmailField(label='Email', widget=TextInput(attrs={'placeholder':'Email'}))
+    first_name = CharField(label='First Name', widget=TextInput(attrs={'placeholder':'First Name'}))
+    last_name = CharField(label='Last Name', widget=TextInput(attrs={'placeholder':'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+        #self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = 'Username'
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>30 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+        self.fields['username'].max_length = 30
+        
+        #self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = 'Password'
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><small><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></small></ul>'
+        
+        #self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = 'Confirm Password'
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
+from .forms import UserRegisterForm
 
 # Create your views here.
 def home(request):
@@ -25,6 +26,28 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged Out.")
     return redirect('home')
+
+def register_user(request):
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have been successfully registered.')
+                return redirect('home')
+            else:
+                messages.warning(request, 'Something went wrong.')
+    context = {
+                'title':'Register',
+                'form':form,          
+    }
+    return render(request, 'crm/register.html', context)
 
 
 
